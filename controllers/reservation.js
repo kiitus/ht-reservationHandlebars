5e890e5ed06f509197be4d6285bd6e089e97cf43
teemu <teemu.hallinen@hotmail.com>
2021-01-23 18:16:03 +0200

    reservationFormation

--- controllers/reservation.js
+++ controllers/reservation.js
@@ -7,40 +7,39 @@ reservationRouter.post("/", (req, res) => {
     var d = new Date(req.body.reservation);
 
     const room = req.body.room.trim()
-  // const year = parseInt(req.body.year)
-  //  const month = parseInt(req.body.month)
-  //  const day = parseInt(req.body.day)
+    // const year = parseInt(req.body.year)
+    //  const month = parseInt(req.body.month)
+    //  const day = parseInt(req.body.day)
     const year = d.getFullYear()
-    const month = d.getMonth() +1
-     const day =d.getDate()
+    const month = d.getMonth() + 1
+    const day = d.getDate()
     const hour = parseInt(req.body.hour)
     const duration = parseInt(req.body.duration)
     const user = req.body.user
 
-    
+
     //Varatut ajat millisekunteina
     let startMil = new Date(year, month - 1, day, hour).getTime()
     let endMil = new Date(year, month - 1, day, hour + duration).getTime()
 
 
-    if(room.trim()==="")
-    {
-        return res.render("main",{layout: 'index', info: "You have to choose class"})
+    if (room.trim() === "") {
+        return res.render("main", { layout: 'index', info: "You have to choose class" })
     }
     //Jos syötteessä virhe
     if (isNaN(startMil) || isNaN(endMil) || (endMil < startMil)) {
-        
-        return res.render('main', {layout: 'index', info: "Error in reservation time"});
-       // return res.status(400).send("Error in reservation time")
+
+        return res.render('main', { layout: 'index', info: "Error in reservation time" });
+        // return res.status(400).send("Error in reservation time")
     }
 
-    time.find({ Room: room }).then((finded_room) => {
+    time.find({ Room: room }).sort({ Starttime: 1 }).then((finded_room) => {
 
         //Tarkistetaan onko huone varattu
-        let reserved = false;
-        reserved = isReserved(res,startMil, endMil, finded_room)
-       
-        if (reserved === false) {
+
+        let ReservedText = isReserved(startMil, endMil, finded_room)
+
+        if (ReservedText.trim() === "") {
             //luodaan uusi varaus
             const time_var = new time({
                 Room: room,
@@ -52,15 +51,19 @@ reservationRouter.post("/", (req, res) => {
             time_var.save().then((saved_time) => {
                 let converted = convertAndGather(saved_time)
                 console.log(converted)
-                res.render('main', {layout: 'index', info:"Reservation added", reservation: converted});
+                res.render('main', { layout: 'index', info: "Reservation added", reservation: converted });
                 //return res.send(converted)
             }).catch((error) => {
                 res.status(400).send(error.message)
             })
         }
         else {
-        //    res.render('main', {layout: 'index', info: "Room was reserved"});
-           // return res.end("Room was reserved")
+            res.render('main', { layout: 'index', info: ReservedText });
+
+            //  res.render('main', {layout: 'index', info: "Class "+reservedTime[0].room+" was reserved "+ reservedTime[0].startTime + " - " +reservedTime[0].endTime});
+
+            //    res.render('main', {layout: 'index', info: "Room was reserved"});
+            // return res.end("Room was reserved")
         }
     })
 
@@ -73,34 +76,34 @@ reservationRouter.post("/delete", (req, res) => {
 
     console.log(id)
     time.findByIdAndDelete(id).then((deleted) => {
-        res.render('main', {layout: 'index', info: "Reservation deleted"});
-      //  res.send("Reservation deleted")
+        res.render('main', { layout: 'index', info: "Reservation deleted" });
+        //  res.send("Reservation deleted")
     }).catch((error) => {
         res.status(400).send("Error in id")
     })
 })
 
 reservationRouter.get("", (req, res) => {
-    
+
 
     //Tähän kerätään hakuehdot
     let query = {};
-    
+
 
     let room = req.query.room
 
-    if (room !== undefined  && room.trim()!=="" ) {
+    if (room !== undefined && room.trim() !== "") {
         let title = "Room";
         let value = req.query.room.trim();
         query[title] = value;
     }
 
-   
+
     //Päiväys pitää olla muodossa ddmmyyyy
 
     let date = req.query.date
-    let searchDay = req.query.searchDay 
-    if (date !== undefined && date.length === 10 && searchDay === "etsi" ) {
+    let searchDay = req.query.searchDay
+    if (date !== undefined && date.length === 10 && searchDay === "etsi") {
         const day = parseInt(req.query.date.substring(8, 10))
         const month = parseInt(req.query.date.substring(5, 7))
         const year = parseInt(req.query.date.substring(0, 4))
@@ -132,7 +135,7 @@ reservationRouter.get("", (req, res) => {
         let valueUser = req.query.user.trim();
         query[titleUser] = valueUser;
     }
-    
+
     console.log(query)
 
 
@@ -141,13 +144,11 @@ reservationRouter.get("", (req, res) => {
 
     time.find(query).sort({ Room: 1, Starttime: 1 }).then((finded) => {
         result = convertAndGather(finded)
-        if(result.length >0)
-        {
-        res.render('main', {layout: 'index', info:"Search results",reservation: result});
+        if (result.length > 0) {
+            res.render('main', { layout: 'index', info: "Search results", reservation: result });
         }
-        else
-        {
-            res.render('main', {layout: 'index', info:"No reservations found"});
+        else {
+            res.render('main', { layout: 'index', info: "No reservations found" });
         }
         //return res.send(result)
     })
@@ -190,48 +191,55 @@ let convertAndGather = (resultToConvert) => {
 }
 
 //Tarkistaa onko luokka varattu, skipillä toteutetaan
-//ettei muokattavan varauksen vanha aika vaikuta tarkastuksessa
-let isReserved = (res,startMil, endMil, times, skip = 0) => {
-    
-    let reserved = false
+//ettei MUOKATTAVA (ei toteutettu UI:ssä) varauksen vanha aika vaikuta tarkastuksessa
+let isReserved = (startMil, endMil, times, skip = 0) => {
+
+    let text = ""
 
     times.forEach((time) => {
 
+
         if (String(time._id) !== skip) {
             if ((time.Starttime <= startMil) && (startMil < time.Endtime)) {
-                reserved = true
-                let reservedTime = convertAndGather(time)
-                console.log(reservedTime)
-                res.render('main', {layout: 'index', info: "Class "+reservedTime[0].room+" was reserved "+ reservedTime[0].startTime + " - " +reservedTime[0].endTime});
+                text = formatText(text,time)
 
             }
             else if ((time.Starttime < endMil) && (endMil <= time.Endtime)) {
-                reserved = true
-                let reservedTime = convertAndGather(time)
-                console.log(reservedTime)
-                res.render('main', {layout: 'index', info: "Class "+reservedTime[0].room+" was reserved "+ reservedTime[0].startTime + " - " +reservedTime[0].endTime});
-
-
+                text = formatText(text,time)
             }
             else if ((startMil <= time.Starttime) && (time.Starttime < endMil)) {
-                reserved = true
-                let reservedTime = convertAndGather(time)
-                console.log(reservedTime)
-                res.render('main', {layout: 'index', info: "Class "+reservedTime[0].room+" was reserved "+ reservedTime[0].startTime + " - " +reservedTime[0].endTime});
-
+             text = formatText(text,time)
             }
             else if ((startMil < time.Endtime) && (time.Endtime <= endMil)) {
-                reserved = true
-                let reservedTime = convertAndGather(time)
-                console.log(reservedTime)
-                res.render('main', {layout: 'index', info: "Class "+reservedTime[0].room+" was reserved "+ reservedTime[0].startTime + " - " +reservedTime[0].endTime});
+            text = formatText(text,time)
 
             }
         }
     })
-    return reserved
+    console.log(text)
+    return text
 }
 
+let formatText = (text, time) => {
+    let reservedTime = convertAndGather(time)
+
+
+    if (text.trim() === "") {
+        text = "Class " + reservedTime[0].room + " was reserved "+ onlyDay(reservedTime[0].startTime)+" <br>"
+    }
+
+    text += removeDay(reservedTime[0].startTime) + " - " + removeDay(reservedTime[0].endTime) + "<br>"
+
+    return text
+}
+let removeDay = (input) => {
+    let remove = input
+    return remove.substring(remove.indexOf(" ") + 1);
+}
+let onlyDay = (input) =>
+{
+    return input.substring(0,input.indexOf(" "));
+}
 
 
 
